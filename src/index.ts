292b94927882cefe9c5c0e904637283d978c5cb5
Tim <timnew.wti@gmail.com>
2018-12-31 18:12:52 +1100

    Make asset loading working

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-import "pixi"
+import * as PIXI from 'pixi.js'
 
-const app = new PIXI.Application({ width: 256, height: 256 })
+import birdImage from './bird.png'
+
+const app = new PIXI.Application({ width: 512, height: 512 })
+
+app.renderer.backgroundColor = 0xffffff
+
 document.body.appendChild(app.view)
+
+PIXI.loader.add([birdImage]).load(() => {
+  const bird = new PIXI.Sprite(PIXI.loader.resources[birdImage].texture)
+
+  app.stage.addChild(bird)
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,28 @@
-const path = require("path");
-const HTMLPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: "./src/index.ts",
-  devtool: "inline-source-map",
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: ['ts-loader'],
         exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|svg)$/,
+        use: ['file-loader']
       }
     ]
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"]
+    extensions: ['.tsx', '.ts', '.js', '.jpg', '.png', '.svg']
   },
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   plugins: [new HTMLPlugin()]
-};
+}
