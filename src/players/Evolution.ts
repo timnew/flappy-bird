7262b34f168062eb4998613b9cfb890daa16e09c
Tim <timnew.wti@gmail.com>
2019-01-06 23:12:33 +1100

    Implement tournament selection

--- src/players/Evolution.ts
+++ src/players/Evolution.ts
@@ -1,15 +1,52 @@
 import AiPlayer from './ai/AiPlayer'
+import { Set } from 'typescript-collections'
+import shuffle from 'lodash.shuffle'
+import maxBy from 'lodash.maxby'
 
-// 1. Fitness should calculate vertical distance from the center of gap on death
-// 2. Input should include vertical velocity
-// 3. Crossover Rate
-// 4. Mutation Rate
-// 5. Crossover Point
+// [X] 1. Fitness should calculate vertical distance from the center of gap on death
+// [X] 2. Input should include vertical velocity
+// [X] 3. Better selection algorithm
+// [ ] 4. Crossover Rate
+// [ ] 5. Mutation Rate
+// [ ] 6. Crossover Point
 
 class Evolution {
-  constructor(readonly players: AiPlayer[]) {}
+  static readonly crossoverRate = 0.8
+  static readonly mutateRate = 0.1
 
-  run(): AiPlayer[] {
-    return []
+  constructor(readonly playerSet: Set<AiPlayer>) {}
+
+  run() {
+    const playerSet = this.playerSet
+    let players = playerSet.toArray()
+    players = this.tournamentSelection(players)
+    players = this.crossover(players)
+    playerSet.clear()
+    players.forEach(playerSet.add.bind(playerSet))
   }
+
+  tournamentSelection(
+    players: AiPlayer[],
+    matchSize: number = 2,
+    matchRound: number = 1
+  ): AiPlayer[] {
+    let remaining = players
+    shuffle(remaining)
+    const selected: AiPlayer[] = []
+
+    while (remaining.length > 0) {
+      const candidates = remaining.slice(0, matchSize)
+      selected.push(maxBy(candidates, p => p.liveScore.overall)!)
+
+      remaining = remaining.slice(matchSize)
+    }
+
+    if (matchRound == 1) {
+      return selected
+    }
+
+    return this.tournamentSelection(selected, matchSize, matchRound - 1)
+  }
+
+  crossover(players: AiPlayer[]): AiPlayer[] {}
 }
