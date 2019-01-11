752dd137fe751406a0f05fe16957e015f338e2e1
Tim <timnew.wti@gmail.com>
2019-01-11 13:29:10 +1100

    Turn AI bird half transparent

--- src/players/ai/AiPlayer.ts
+++ src/players/ai/AiPlayer.ts
@@ -2,10 +2,16 @@ import Player from '../Player'
 import Gene from './Gene'
 import { typedName } from '../../engine/Name'
 import PlayerVisual from '../PlayerVisual'
+import Bird from '../../world/Bird'
 
 export default class AiPlayer extends Player {
   constructor(readonly gene: Gene) {
     super(typedName('AI', gene.name))
+  }
+
+  attach(bird: Bird) {
+    super.attach(bird)
+    bird.birdSprite.alpha = 0.3
   }
 
   onVisual(visual: PlayerVisual) {
